89322c15ca8961a98249fad065d635049de19b02
Samantha CL Ho <32963760+saclho@users.noreply.github.com>
2018-07-19 12:53:12 -0700

    Beginning JS element
    getting somewhere with js; missing some gallery images-- about to replace

---
+++ saho.studio/scripts/scroll.js
@@ -0,0 +1,17 @@
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// loop through all elements
+$('.fade-in').each(function() {
+  
+  // build a tween
+  var tween = TweenMax.from($(this), 0.3, {autoAlpha: 0, scale: 0.5, y: '+=30', ease:Linear.easeNone});
+
+  // build a scene
+  var scene = new ScrollMagic.Scene({
+    triggerElement: this
+  })
+  .setTween(tween) // trigger a TweenMax.to tween
+  .addTo(controller);
+  
+});
